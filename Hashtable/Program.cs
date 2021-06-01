304ef652ff1a5a1cd1b63b7459a74b0ce56cd2e0
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-01 19:09:24 +0530

    Add files via upload

---
+++ Hashtable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace Generic_Hashtable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("1", ".Net");
+            ht.Add("2", "C#");
+            ht.Add("3", "ASP.Net");
+            ht.Remove("1");
+            ICollection keys = ht.Keys;
+            foreach (String k in keys)
+            {
+                Console.WriteLine(ht[k]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
